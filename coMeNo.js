195c0fb8dd2acfdba357dde88e3017a0d8da4542
radoslaw.ch <radoslaw.ch@outlook.com>
2019-02-12 19:34:53 +0100

    Better behaviour with non-standard input fields (copies data to clipboard when detecting such field). Improved options page (editing multi-line notes, adding notes).

--- coMeNo.js
+++ coMeNo.js
@@ -4,16 +4,15 @@ function checkError(error){
   }
 }
 
-
+// purge removed notes from previous Firefox sessoin
 browser.storage.sync.remove('removedNotesArray');
 
-
-//message to content script
+// sends a message to the content script
 function sendToActiveTab(message){
     chrome.tabs.query({
         windowId : chrome.windows.WINDOW_ID_CURRENT,
         active : true
-    }, function(tabs) {     
+    }, function(tabs) {
         for (var i=0; i<tabs.length; ++i) {
                 chrome.tabs.sendMessage(tabs[i].id, message,checkError);
             }
@@ -22,7 +21,8 @@ function sendToActiveTab(message){
 }
 
 
-//message from content script
+// receives a message from te content script
+// and reacts to it
 chrome.runtime.onMessage.addListener(
     function(message, sender, sendResponse) {
         if(message.name == "noteCopied"){
@@ -30,7 +30,7 @@ chrome.runtime.onMessage.addListener(
                 {
                     "type": "basic",
                     "title": "coMeNo",
-                    "message": "Note was copied to clipboard."
+                    "message": message.messageOverride || "Note was copied to clipboard."
                 }
             )
         }else if (message.name == "refreshCoMeNo"){
@@ -42,26 +42,32 @@ chrome.runtime.onMessage.addListener(
 
 chrome.storage.sync.get("notesArray", populateCoMeNoFromStorage);
 
-//generates context menu
+// generates context menu
 function populateCoMeNoFromStorage(result){
     chrome.contextMenus.removeAll();
-    
+
+
     chrome.contextMenus.create({
           id: "saveNote",
-          title: "~Save note~",
+          title: "~save note~",
           contexts: ["link", "selection"]
+    });
+    
+    chrome.contextMenus.create({
+          id: "optionsPage",
+          title: "~edit notes~"
     });
     
     if(result.notesArray){
         notesArray = result.notesArray;
         populateCoMeNoFromVariable();
-        
+
     }else{
         notesArray = new Array();
-    } 
+    }
 }
 
-//populates context menu with notes
+// populates context menu with notes
 function populateCoMeNoFromVariable(){
     chrome.contextMenus.create({
         id: "separator",
@@ -73,12 +79,12 @@ function populateCoMeNoFromVariable(){
           id: notesArray[i].id.toString(),
           title: notesArray[i].content.slice(0,24),
           contexts: ["editable", "page"]
-        }); 
+        });
     }
 }
 
-//updates context menu with a new note
-function addNewCoMeNo(){ 
+// updates context menu with a new note
+function addNewCoMeNo(){
     chrome.contextMenus.create({
         id: notesArray[notesArray.length-1].id.toString(),
         title: notesArray[notesArray.length-1].content.slice(0,24),
@@ -86,54 +92,56 @@ function addNewCoMeNo(){
     });
 }
 
-
-
 chrome.contextMenus.onClicked.addListener(onClicked);
 
+// reacts on context menu click
 function onClicked(info, tab){
     for(var i = 0; i < notesArray.length; i++){
         if(notesArray[i].id == info.menuItemId)break;
     }
-    
+
     if(info.menuItemId == "saveNote"){
-        // if user pressed option to save new note
-        if(info.selectionText != "" && typeof info.selectionText != "undefined"){
-            // saving selection
-            content = info.selectionText;
-        }else if(info.linkUrl != "" && typeof info.linkUrl != "undefined"){
-            // saving url
-            content = info.linkUrl;
-        }else{
-            return;
-        }
-        
-        if(notesArray.length > 0){
-            // if any not exist add new with id one higher than last element of 
-            // notesArray
-            notesArray[notesArray.length] = {
-                id: notesArray[notesArray.length-1].id+1,
-                content: content
-            };
-        }else{
-            // if there are no notes yet add new with id = 0
-            notesArray[notesArray.length] = {
-                id: 0,
-                content: content
-            };           
-        }        
-        chrome.storage.sync.set({notesArray: notesArray}, addNewCoMeNo); 
+      // saving selection as a new note
+      if(info.selectionText != "" && typeof info.selectionText != "undefined"){
+          // saving selection
+          content = info.selectionText;
+      }else if(info.linkUrl != "" && typeof info.linkUrl != "undefined"){
+          // saving url
+          content = info.linkUrl;
+      }else{
+          return;
+      }
+
+      if(notesArray.length > 0){
+          // if any not exist add new with id one higher than last element of
+          // notesArray
+          notesArray[notesArray.length] = {
+              id: notesArray[notesArray.length-1].id+1,
+              content: content
+          };
+      }else{
+          // if there are no notes yet add new with id = 0
+          notesArray[notesArray.length] = {
+              id: 0,
+              content: content
+          };
+      }
+      chrome.storage.sync.set({notesArray: notesArray}, addNewCoMeNo);
+    }else if(info.menuItemId == "optionsPage"){
+      // opening options page
+      browser.runtime.openOptionsPage();
     }else if(info.editable){
         // if user pressed existing note
-		// paste it into editable field
+        // paste it into editable field
         sendToActiveTab({
-			name: "paste",
-			content: notesArray[i].content
-		});
+        name: "paste",
+        content: notesArray[i].content
+      });
     }else{
-        // or copy it to clipboard
+        // or copy it to clipboard if the click wasn't over an editable field
         sendToActiveTab({
-			name: "copy",
-			content: notesArray[i].content
-		});
+        name: "copy",
+        content: notesArray[i].content
+      });
     }
 };
--- coMeNoCs.js
+++ coMeNoCs.js
@@ -8,28 +8,90 @@ function checkError(error){
 chrome.runtime.onMessage.addListener(pasteOrCopy);
 
 function pasteOrCopy(message, sender, sendResponse){
-	if(message.name == "paste"){
-		//seems to work on standard input elements
-		// doesn't work in (for ex.) FB Messenger input field
-		actEl = document.activeElement;
-		selStart = actEl.selectionStart;
-		selStartCopy = selStart;
-		selEnd = actEl.selectionEnd;	  
-		actEl.value = actEl.value.slice(0,selStart) + message.content + actEl.value.slice(selEnd);	  
-		document.activeElement.selectionStart = selStartCopy + message.content.length;
-		document.activeElement.selectionEnd = selStartCopy + message.content.length;
-        
-	}else if(message.name == "copy"){
-		var coMeNoHiddenInput = document.createElement("input");
-		coMeNoHiddenInput.setAttribute("type", "collapse");
-		coMeNoHiddenInput.setAttribute("id", "coMeNoHiddenInput");
-		coMeNoHiddenInput.setAttribute("value", message.content);
-		document.body.appendChild(coMeNoHiddenInput);
-		coMeNoHiddenInput.select();
-		document.execCommand("copy");
-		coMeNoHiddenInput.remove();
-		chrome.runtime.sendMessage({
-		   name: "noteCopied"
-		},checkError);
-	}
+  if(message.name == "paste"){
+    paste(message);
+   
+  }else if(message.name == "copy"){
+    copy(message);
+  }
 }
+
+function paste(message){
+  actEl = getActiveElement();
+  // seems to work on standard input elements
+  // doesn't work in (for ex.) FB Messenger input field
+  // so check if we are dealing with normal input field
+  var nodeName = actEl.nodeName.toLowerCase();
+
+  // if making an attempt to paste into normal input field or textarea
+  // find current selection and place the note in it
+  // copy to clipboard otherwise
+  if ((nodeName == "input" && /^(?:text|email|number|search|tel|url|password)$/i.test(actEl.type)) || nodeName == "textarea") { 
+    selStart = actEl.selectionStart;
+    selStartCopy = selStart;
+    selEnd = actEl.selectionEnd;
+    if(actEl.hasAttribute("value") || actEl.innerHTML =="" ){
+      intendedValue = actEl.value.slice(0,selStart) + message.content + actEl.value.slice(selEnd);
+      actEl.value = intendedValue;
+    }else{
+      intendedValue = actEl.innerHTML.slice(0,selStart) + message.content + actEl.innerHTML.slice(selEnd);
+      actEl.innerHTML = intendedValue;
+    }
+    actEl.selectionStart = selStartCopy + message.content.length;
+    actEl.selectionEnd = selStartCopy + message.content.length;
+  }else{
+    let messageOverride = "It's impossible to input into non-standard input field, sorry. :-(\n" +
+      "Copied your note into the clipboard insted. You can paste it yourself now!"
+    copy(message, messageOverride);
+  }
+}
+
+
+// copy the note to the clipboard by
+// creating an invisible input field
+// writing the note to it and then executing
+// the copy command
+function copy(message, messageOverride){
+  let originalElement = getActiveElement();
+  var coMeNoHiddenInput = document.createElement("input");
+  coMeNoHiddenInput.setAttribute("type", "collapse");
+  coMeNoHiddenInput.setAttribute("id", "coMeNoHiddenInput");
+  coMeNoHiddenInput.setAttribute("value", message.content);
+  document.body.appendChild(coMeNoHiddenInput);
+  coMeNoHiddenInput.select();
+  document.execCommand("copy");
+  coMeNoHiddenInput.remove();
+  chrome.runtime.sendMessage({
+     name: "noteCopied",
+     messageOverride: messageOverride || false
+  },checkError);
+  originalElement.focus();
+}
+
+// https://stackoverflow.com/a/25420726
+/**
+* Retrieve active element of document and preserve iframe priority MULTILEVEL!
+* @return HTMLElement
+**/
+var getActiveElement = function( document ){
+
+     document = document || window.document;
+
+     // Check if the active element is in the main web or iframe
+     if( document.body === document.activeElement
+        || document.activeElement.tagName == 'IFRAME' ){
+         // Get iframes
+         var iframes = document.getElementsByTagName('iframe');
+         for(var i = 0; i<iframes.length; i++ ){
+             // Recall
+             var focused = getActiveElement( iframes[i].contentWindow.document );
+             if( focused !== false ){
+                 return focused; // The focused
+             }
+         }
+     }else{
+       return document.activeElement;
+     }
+
+     return false;
+};
